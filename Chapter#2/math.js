454ee1c7256f0bb72885d5a20f8aa1583c7b130c
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2020-09-13 05:37:30 +0600

    commited math.js

---
+++ Chapter#2/math.js
@@ -0,0 +1,15 @@
+
+var n = 5.489
+
+console.log(Math.abs(n))
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+console.log(Math.max(500,600,700))
+console.log(Math.min(200,300,400))
+console.log(Math.pow(2,3))
+console.log(Math.pow(3,5))
+console.log(Math.sqrt(9))
+console.log(Math.sqrt(64))
+
+console.log(Math.round( Math.random() * 50 + 1))
