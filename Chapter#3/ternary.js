60523ff971fdb0b993a7eda351bfd7e5dd6d0d25
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2020-09-14 04:35:31 +0600

    ternary.js file commited

---
+++ Chapter#3/ternary.js
@@ -0,0 +1,15 @@
+
+var n = 11
+
+// var str = ''
+
+// if (n % 2 === 0) {
+//     str = 'EVEN'
+// } else {
+//     str = 'ODD'
+// }
+
+var result = n % 2 === 0 ? 'EVEN' : 'ODD'
+
+console.log(result)
+
