c95ea8acb4f79d112b88ff306435736fd5ddaa12
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2020-09-14 04:20:05 +0600

    If and switch commited

---
+++ Chapter#3/if.js
@@ -0,0 +1,24 @@
+
+var a = 20
+var b = 20
+
+if (a > b) {
+    console.log(a + ' is grater than ' + b)
+}
+else if (a < b) {
+    console.log(b + ' is grater than ' + a)
+} else {
+    console.log('They Both are Same')
+}
+
+var n = 0
+
+if (n === 0) {
+    console.log('N is Zero')
+} else if (n % 2 === 0) {
+    console.log(n + ' is Even number')
+}
+else {
+    console.log(n + ' is Odd number')
+}
+
